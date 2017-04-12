b8b77e97c0677c5f9320637bf6986a31e6b63a67
Connor Bär <hello@connorbaer.co>
2017-04-12 13:24:47 +0200

    Customisations for madebyconnor.co.

--- public/service-worker.js
+++ public/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["assets/css/amp.min.css","342cef184f8c7ae57dfb4a1715809417"],["assets/css/home.min.css","7fe06d2a81359d3fdf122fe85e2a59b8"],["assets/css/styles.min.css","cf6f11c4efdc37a0f27acc0c5805850f"],["assets/js/home.min.js","85dfa7d01ca2e17647e74bd1e76e5a8e"],["assets/js/photoswipe.min.js","cc587507c916468d78f3cba9e491639e"],["assets/js/prism.min.js","f1ab1f831161beabcf188467c71a116d"],["assets/js/scripts.min.js","c9cc17f96e7daf0070b8c88de4e826a1"]];
+var precacheConfig = [["assets/css/amp.min.css","9eca0c2d45d6242883ad7f9a2ed15494"],["assets/css/home.min.css","7fe06d2a81359d3fdf122fe85e2a59b8"],["assets/css/styles.min.css","5315a2bc072428035fad0660bf2b81cc"],["assets/js/home.min.js","85dfa7d01ca2e17647e74bd1e76e5a8e"],["assets/js/photoswipe.min.js","cc587507c916468d78f3cba9e491639e"],["assets/js/prism.min.js","f1ab1f831161beabcf188467c71a116d"],["assets/js/scripts.min.js","c9cc17f96e7daf0070b8c88de4e826a1"]];
 var cacheName = 'sw-precache-v3-connorbaer-' + (self.registration ? self.registration.scope : '');
 
 
