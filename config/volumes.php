c176b08de48f892989d011e5b21d12442395f8a0
Connor Bär <hello@connorbaer.co>
2017-04-28 09:32:32 +0200

    New volume for user photos.

--- config/volumes.php
+++ config/volumes.php
@@ -1,16 +1,20 @@
 <?php
 
 return [
-		'images' => [
-				'path' => getenv( 'VOLUMES_BASE_PATH' ) . 'img',
-				'url' => getenv( 'VOLUMES_BASE_URL' ) . 'img',
-		],
-		'videos' => [
-				'path' => getenv( 'VOLUMES_BASE_PATH' ) . 'vid',
-				'url' => getenv( 'VOLUMES_BASE_URL' ) . 'vid',
-		],
-		'documents' => [
-				'path' => getenv( 'VOLUMES_BASE_PATH' ) . 'doc',
-				'url' => getenv( 'VOLUMES_BASE_URL' ) . 'doc',
-		],
+    'users' => [
+        'path' => getenv('VOLUMES_BASE_PATH') . 'usr',
+        'url' => getenv('VOLUMES_BASE_URL') . 'usr',
+    ],
+    'images' => [
+        'path' => getenv('VOLUMES_BASE_PATH') . 'img',
+        'url' => getenv('VOLUMES_BASE_URL') . 'img',
+    ],
+    'videos' => [
+        'path' => getenv('VOLUMES_BASE_PATH') . 'vid',
+        'url' => getenv('VOLUMES_BASE_URL') . 'vid',
+    ],
+    'documents' => [
+        'path' => getenv('VOLUMES_BASE_PATH') . 'doc',
+        'url' => getenv('VOLUMES_BASE_URL') . 'doc',
+    ],
 ];
