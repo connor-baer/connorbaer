c99aeb5b30ee3ab2dd4aaf0d201d13125e3e2d70
Connor Bär <connor.baer@sumup.com>
2017-09-12 11:18:32 +0200

    Add api endpoint for globals

--- config/element-api.php
+++ config/element-api.php
@@ -11,6 +11,7 @@
 use craft\elements\Entry;
 use craft\elements\Category;
 use craft\elements\Tag;
+use craft\elements\GlobalSet;
 
 return [
     'defaults' => [
@@ -19,6 +20,68 @@ return [
     ],
 
     'endpoints' => [
+
+      'api/global/<handle:{handle}.json>' => function($handle) {
+          return [
+              'elementType' => GlobalSet::class,
+              'criteria' => ['handle' => $handle],
+              'first' => true,
+              'transformer' => function(GlobalSet $globalSet) {
+                  $links = [];
+
+                  foreach ($globalSet->navigation as $block) {
+
+                     switch ($block->type->handle) {
+                         default:
+                             $links[] = [
+                              'label' => $block->type->navTitle,
+                              'link' => $block->type->navLink
+                             ];
+                     }
+                  }
+
+                  return [
+                      'description' => $globalSet->description,
+                      'links' => $links
+                  ];
+              }
+          ];
+        }
+
+        'api/global/recipes.json' => [
+            'elementType' => Entry::class,
+            'criteria' => [
+                'section' => 'food',
+                'orderBy' => 'title',
+            ],
+            'transformer' => function (Entry $entry) {
+                $measure = Craft::$app->request->getQueryParam('measure');
+                $people = Craft::$app->request->getQueryParam('people');
+
+                $ingredients = [];
+
+                $labels = [];
+
+                foreach ($entry['recipe']->getIngredients($measure, $people) as $ingredient) {
+                    $ingredients[] = $ingredient;
+                }
+
+                foreach ($entry['labels'] as $label) {
+                    $labels[] = $label['title'];
+                }
+
+                return [
+                    'title' => $entry['title'],
+                    'description' => $entry['recipe']['description'],
+                    'image' => $entry['recipe']->getImageUrl(),
+                    'labels' => implode(', ', $labels),
+                    'ingredients' => $ingredients,
+                    'skill' => ucfirst($entry['recipe']['skill']),
+                    'totalTime' => $entry['recipe']['totalTime'],
+                    'slug' => $entry['slug'],
+                ];
+            }
+        ],
 
         'api/categories/<categoryGroup:{slug}>.json' => function ($categoryGroup) {
             return [
