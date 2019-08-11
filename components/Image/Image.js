b12160b70436a046a65133354e8cccc28ad431b3
Connor Bär <connor.baer@sumup.com>
2019-08-11 14:50:06 +0200

    fix(all): Add aspect ratio to more images

--- components/Image/Image.js
+++ components/Image/Image.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Image as BambooImage, sharedPropTypes } from '@madebyconnor/bamboo-ui';
 
+const DEFAULT_WIDTH = 1200;
+
+function formatSrc(src, width) {
+  return `${src}?w=${width}`;
+}
+
 function formatSrcSet(src, srcSet = [400, 800, 1200, 1600, 2000]) {
-  return srcSet.map(set => `${src}?w=${set} ${set}w`).join(', ');
+  return srcSet.map(width => `${formatSrc(src, width)} ${width}w`).join(', ');
 }
 
 export default function Image(props) {
+  const width = props.width || DEFAULT_WIDTH;
+  const src = formatSrc(props.src, width);
   const srcSet = formatSrcSet(props.src, props.srcSet);
-  return <BambooImage {...props} srcSet={srcSet} />;
+  return <BambooImage {...props} src={src} srcSet={srcSet} />;
 }
 
 Image.propTypes = sharedPropTypes.imagePropType;
