5c84854ac603f3e54a5b9b83edeb1d1d09cba58f
Connor Bär <connor.baer@me.com>
2020-11-24 02:23:10 +0100

    Add Prisma schema

---
+++ prisma/client.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PrismaClient, PrismaClientOptions } from '@prisma/client';
+
+// Prevent Prisma from opening too many database connections in local development
+// From https://next-auth.js.org/schemas/adapters#custom-models
+
+// eslint-disable-next-line import/no-mutable-exports
+let prisma: PrismaClient<PrismaClientOptions, never>;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // @ts-expect-error prisma is a custom global
+  if (!global.prisma) {
+    // @ts-expect-error prisma is a custom global
+    global.prisma = new PrismaClient();
+  }
+  // @ts-expect-error prisma is a custom global
+  prisma = global.prisma;
+}
+
+export { prisma };
